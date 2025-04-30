fb62049554f00a7bb8b9cd3af1a85e422b5a3bdc
Erick2621 <erickalonso2619@hotmail.com>
2025-04-30 15:53:01 -0500

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,6 @@
+// import {crearDeck as crearNuevoDeck} from './usecases/crear-deck';
+export  { pedirCarta } from './pedir-carta';
+export  { valorCarta } from './valor-carta';
+export { crearDeck } from './crear-deck';
+export {crearCartaHTML} from './crear-carta-html'
+export {turnoComputadora} from './turno-computadora';
